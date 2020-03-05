3c1a20303f0f61630e15459c8f3ead0fd50a618c
Jorge Ramirez <jorger99@umd.edu>
2020-03-05 18:43:15 -0500

    synced lab2 to dropbox

--- phys375_optics_lab/lab2/laser_profile.m
+++ phys375_optics_lab/lab2/laser_profile.m
@@ -1,13 +1,13 @@
 % this script will measure the laser profile
 
-function data = laser_profile(distance)
+function data = laser_profile(distance, scan_mm)
 
-mm_movement = 19;  % how many mm to move
+%mm_movement = 5;  % how many mm to move
 conversion = 10000; % 10,000 steps per mm
 
-laser_data = collect_data(mm_movement * conversion)
+laser_data = collect_data(scan_mm * conversion)
 
-filename = "data_partB/distance_" + distance + "mm.txt";
+filename = "data_partE/distance_" + distance + "mm.txt";
 save(filename, 'laser_data', '-ascii');
 
 end
